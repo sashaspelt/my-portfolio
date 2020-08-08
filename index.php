3c904cc14ef40e95c7a72cb6e854a63c2131a4ca
unknown <sasha.spelt@gmail.com>
2020-08-09 03:11:05 +1000

    Homepage design

--- index.php
+++ index.php
@@ -1,1 +1,38 @@
-Sasha Spelt - Web Developer
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap" rel="stylesheet"> 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="./css/stylesheet.css"> 
+<script type="text/javascript" src="index.js"></script>
+
+</head>
+<body>
+
+<header>
+<h1>sasha spelt</h1>
+<h2>web development</h2>
+</header>
+
+<nav id="responsiveNav">
+    <a href="#">home</a>
+    <a href="#">projects</a>
+    <a href="#">contact</a>
+    <a href="https://www.linkedin.com/in/sasha-spelt-1223b0120/" target="_blank" class="socialicons"><i class="fa fa-linkedin"></i></a>
+    <a href="https://github.com/sashaspelt" target="_blank" class="socialicons"><i class="fa fa-github"></i></a>
+    <a href="javascript:void(0);" class="icon" onclick="myFunction()"><i class="fa fa-bars"></i></a>
+
+
+</nav>
+
+<main>
+</main>
+
+<footer>
+</footer>
+
+</body>
+
+</html>
